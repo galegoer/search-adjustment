b2ba1115fb04a51d133a27e472ea161dec11dbfa
galegoer <eric.m.galego@gmail.com>
2025-01-28 17:09:03 -0500

    fixing chrome request

--- background.js
+++ background.js
@@ -1,34 +1,48 @@
 /**
  * Listens for outgoing requests to Google and modifies the query parameter.
 */
-browser.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        const url = new URL(details.url);
 
-        // // Check if the request has already been processed
-        // if (processedRequests.has(details.requestId)) {
-        //     return;
-        // }
+// chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(async (e) => {
+chrome.webRequest.onBeforeRequest.addListener( async (e) => {
+    console.log('hii');
+    const url = new URL(e.url);
+    // // Check if the request has already been processed
+    // if (processedRequests.has(details.requestId)) {
+    //     return;
+    // }
 
-        // Check if the URL includes a search query
-        if (url.hostname.includes("google.com") && url.searchParams.has("q")) {
-            // Add a keyword to the search query
-            const currentQuery = url.searchParams.get("q");
-            const keyword = "-ai";
-            if (!currentQuery.includes(keyword)) {
-                console.log(currentQuery);
-                const modifiedQuery = `${currentQuery} ${keyword}`; // Replace 'yourKeyword' with your desired keyword
-                url.searchParams.set("q", modifiedQuery);
+    // Check if the URL includes a search query
+    if (url.hostname.includes("google.com") && url.searchParams.has("q")) {
+        // Add a keyword to the search query
+        const currentQuery = url.searchParams.get("q");
+        console.log(currentQuery);
+        const keywords = await getKeywords();
+        console.log(keywords);
+        if (!currentQuery.includes(keywords)) {
+            console.log(currentQuery);
+            const modifiedQuery = `${currentQuery} ${keyword}`; // Replace 'yourKeyword' with your desired keyword
+            url.searchParams.set("q", modifiedQuery);
 
-                return { redirectUrl: url.toString() }; // Redirect to the modified URL
-            } else {
-                console.log('here');
-                return;
-            }
+            return { redirectUrl: url.toString() }; // Redirect to the modified URL
+        } else {
+            console.log('here');
+            return;
         }
+    }
     },
     {
         urls: ["*://*.google.com/search*"], // Listen only for Google search requests
     },
     ["blocking"] // Ensure the request is blocked and modified before it proceeds
 );
+
+async function getKeywords() {
+    return new Promise(resolve => {
+        let keywords = [];
+        chrome.storage.local.get(['ai-search'], items => {
+            console.log(items);
+            keywords['ai-search'] = items['ai-search'];
+            resolve(keywords);
+        });
+    })
+}
